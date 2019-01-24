3e0f45c2858cc6f16fe8564c0a7020c18f719f2a
Nikolay <nikolas-ja@meta.ua>
2019-01-24 11:24:36 +0200

    summernote without picture and video

--- publish/admin/js/jquery.editorSummernote.js
+++ publish/admin/js/jquery.editorSummernote.js
@@ -5667,20 +5667,20 @@
                     click: _this.context.createInvokeHandler('linkDialog.show')
                 }).render();
             });
-            this.context.memo('button.picture', function () {
-                return _this.button({
-                    contents: _this.ui.icon(_this.options.icons.picture),
-                    tooltip: _this.lang.image.image,
-                    click: _this.context.createInvokeHandler('imageDialog.show')
-                }).render();
-            });
-            this.context.memo('button.video', function () {
-                return _this.button({
-                    contents: _this.ui.icon(_this.options.icons.video),
-                    tooltip: _this.lang.video.video,
-                    click: _this.context.createInvokeHandler('videoDialog.show')
-                }).render();
-            });
+            // this.context.memo('button.picture', function () {
+            //     return _this.button({
+            //         contents: _this.ui.icon(_this.options.icons.picture),
+            //         tooltip: _this.lang.image.image,
+            //         click: _this.context.createInvokeHandler('imageDialog.show')
+            //     }).render();
+            // });
+            // this.context.memo('button.video', function () {
+            //     return _this.button({
+            //         contents: _this.ui.icon(_this.options.icons.video),
+            //         tooltip: _this.lang.video.video,
+            //         click: _this.context.createInvokeHandler('videoDialog.show')
+            //     }).render();
+            // });
             this.context.memo('button.hr', function () {
                 return _this.button({
                     contents: _this.ui.icon(_this.options.icons.minus),
