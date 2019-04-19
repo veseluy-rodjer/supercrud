83b4c92dbfe1c555e55859ee01330ded43070d89
Nikolay <nikolas-ja@meta.ua>
2019-04-19 16:34:00 +0300

    add toogle locale into header

--- publish/views/admin/layouts/app.blade.php
+++ publish/views/admin/layouts/app.blade.php
@@ -61,14 +61,15 @@
 <div class="navbar navbar-default" role="navigation">
 
     <div class="navbar-inner">
-        <button type="button" class="navbar-toggle pull-left animated flip">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.html"> <img alt="Charisma Logo" src="{{ asset('admin/img/logo20.png') }}" class="hidden-xs"/>
-            <span>Charisma</span></a>
+        {{-- <button type="button" class="navbar-toggle pull-left animated flip"> --}}
+            {{-- <span class="sr-only">Toggle navigation</span> --}}
+            {{-- <span class="icon-bar"></span> --}}
+            {{-- <span class="icon-bar"></span> --}}
+            {{-- <span class="icon-bar"></span> --}}
+        {{-- </button> --}}
+		<div class="navbar-brand">
+			<img alt="Charisma Logo" src="{{ asset('admin/img/logo20.png') }}" class="hidden-xs"/> <span>Charisma</span>
+		</div>
 
         <!-- user dropdown starts -->
         <div class="btn-group pull-right">
@@ -90,6 +91,9 @@
 
         <!-- theme selector starts -->
         <div class="btn-group pull-right theme-container animated tada">
+			<div class="btn-group" style="float: left">
+				@include('locales.locale')			
+			</div>
             <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
                 <i class="glyphicon glyphicon-tint"></i><span
                     class="hidden-sm hidden-xs"> Change Theme / Skin</span>
@@ -111,25 +115,25 @@
 
         <ul class="collapse navbar-collapse nav navbar-nav top-menu">
             <li><a href="{{ env("APP_URL") }}"><i class="glyphicon glyphicon-globe"></i> Visit Site</a></li>
-            <li class="dropdown">
-                <a href="#" data-toggle="dropdown"><i class="glyphicon glyphicon-star"></i> Dropdown <span
-                        class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                </ul>
-            </li>
-            <li>
-                <form class="navbar-search pull-left">
-                    <input placeholder="Search" class="search-query form-control col-md-10" name="query"
-                           type="text">
-                </form>
-            </li>
+            {{-- <li class="dropdown"> --}}
+                {{-- <a href="#" data-toggle="dropdown"><i class="glyphicon glyphicon-star"></i> Dropdown <span --}}
+                        {{-- class="caret"></span></a> --}}
+                {{-- <ul class="dropdown-menu" role="menu"> --}}
+                    {{-- <li><a href="#">Action</a></li> --}}
+                    {{-- <li><a href="#">Another action</a></li> --}}
+                    {{-- <li><a href="#">Something else here</a></li> --}}
+                    {{-- <li class="divider"></li> --}}
+                    {{-- <li><a href="#">Separated link</a></li> --}}
+                    {{-- <li class="divider"></li> --}}
+                    {{-- <li><a href="#">One more separated link</a></li> --}}
+                {{-- </ul> --}}
+            {{-- </li> --}}
+            {{-- <li> --}}
+                {{-- <form class="navbar-search pull-left"> --}}
+                    {{-- <input placeholder="Search" class="search-query form-control col-md-10" name="query" --}}
+                           {{-- type="text"> --}}
+                {{-- </form> --}}
+            {{-- </li> --}}
         </ul>
 
     </div>
