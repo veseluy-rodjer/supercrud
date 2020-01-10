676d200beff0754ebd71bac6bd49466d26864eba
Nikolay <nikolay.m@filancy.com>
2020-01-10 16:41:42 +0200

    Flag of countries with select language

--- publish/views/locales/locale.blade.php
+++ publish/views/locales/locale.blade.php
@@ -1,3 +1,32 @@
+{{-- In /resources/views/layouts/app.blade.php
+	include
+	    <!-- Styles flag-icon-->
+	    <link href="{{ asset('flag-icon-css-master/css/flag-icon.css') }}" rel="stylesheet">
+--}}
+
+<div class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+		<span class="flag-icon flag-icon-{{ \App::getLocale() }}"></span>
+    </a>
+    <div class="dropdown-menu">
+		@foreach (config('languages.languages') as $lang)
+			<a class="dropdown-item" href="#" onclick="selectLanguage(this); event.preventDefault()" data-lang="{{ $lang }}"><span class="flag-icon flag-icon-{{ $lang }}"></span></a>
+		@endforeach
+    </div>
+</div>
+
+<script>
+// change language
+function selectLanguage(e) {
+	let lang = e.dataset.lang;
+	let route = "{{ route('setlocale') }}";
+	document.location.href = route + '/' + lang + '/';
+}
+</script>
+
+{{-- Change language by select tag --}}
+@php
+	/*
 <p>
 	<select id="select-language" name="language">
 		@foreach (config('languages.languages') as $lang)
@@ -15,3 +44,5 @@ selectLanguage.onchange = function(e) {
 	document.location.href = route + '/' + lang + '/';
 } 
 </script>
+	*/
+@endphp
