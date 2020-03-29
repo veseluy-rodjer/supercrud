29b794536a04f724c331992471f0bb81dd0db9b6
Nikolay <nikolas_@mail.ru>
2020-03-29 19:15:41 +0300

    tasks

--- src/Commands/CrudCommand.php
+++ src/Commands/CrudCommand.php
@@ -128,7 +128,7 @@ class CrudCommand extends Command
         $formHelper = $this->option('form-helper');
         $softDeletes = $this->option('soft-deletes');
 
-        $this->call('crud:controller', ['name' => $controllerNamespace . str_singular($name) . 'Controller', '--crud-name' => $name, '--model-name' => $modelName, '--model-namespace' => $modelNamespace, '--view-path' => $viewPath, '--route-group' => $routeGroup, '--pagination' => $perPage, '--fields' => $fields, '--validations' => $validations]);
+        $this->call('crud:controller', ['name' => $controllerNamespace . $name . 'Controller', '--crud-name' => $name, '--model-name' => $modelName, '--model-namespace' => $modelNamespace, '--view-path' => $viewPath, '--route-group' => $routeGroup, '--pagination' => $perPage, '--fields' => $fields, '--validations' => $validations]);
         $this->call('crud:model', ['name' => $modelNamespace . $modelName, '--fillable' => $fillable, '--table' => $tableName, '--pk' => $primaryKey, '--relationships' => $relationships, '--soft-deletes' => $softDeletes]);
         $this->call('crud:migration', ['name' => $migrationName, '--schema' => $migrationFields, '--pk' => $primaryKey, '--indexes' => $indexes, '--foreign-keys' => $foreignKeys, '--soft-deletes' => $softDeletes]);
         $this->call('crud:view', ['name' => $name, '--fields' => $fields, '--validations' => $validations, '--view-path' => $viewPath, '--route-group' => $routeGroup, '--localize' => $localize, '--pk' => $primaryKey, '--form-helper' => $formHelper]);
@@ -158,7 +158,7 @@ class CrudCommand extends Command
         }
 
         if (file_exists($routeFile) && (strtolower($this->option('route')) === 'yes')) {
-            $this->controller = ($controllerNamespace != '') ? $controllerNamespace . str_singular($name) . 'Controller' : str_singular($name) . 'Controller';
+            $this->controller = ($controllerNamespace != '') ? $controllerNamespace . $name . 'Controller' : $name . 'Controller';
 
             $isAdded = File::append($routeFile, "\n" . implode("\n", $this->addRoutes()));
 
