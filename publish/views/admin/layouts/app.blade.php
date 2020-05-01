046895da509f415a299ffb0827bd7f066ffde2e1
Nikolay <nikolas_@mail.ru>
2020-05-01 03:09:01 +0300

    index style button

--- publish/views/admin/layouts/app.blade.php
+++ publish/views/admin/layouts/app.blade.php
@@ -6,6 +6,7 @@
   <title>AdminLTE 3 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
 	@yield('styles')
