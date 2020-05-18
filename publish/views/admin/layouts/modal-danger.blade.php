c73a09de216b8ce32d2a04648e85d0ca1e1ceb43
Nikolay <nikolas_@mail.ru>
2020-05-19 01:27:08 +0300

    modal delete

---
+++ publish/views/admin/layouts/modal-danger.blade.php
@@ -0,0 +1,23 @@
+{{-- Modal for delete model --}}
+<div class="modal fade" id="modal-danger">
+	<div class="modal-dialog">
+	  <div class="modal-content bg-danger">
+		<div class="modal-header">
+		  <h4 style="text-align: center;" class="modal-title">{{ $user->name ?? 'Stranger'}}!</h4>
+		  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		  </button>
+		</div>
+		<div class="modal-body">
+		  <p style="text-align: center">Confirm delete</p>
+		</div>
+		<div class="modal-footer justify-content-between">
+		  <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+		  <button form="" type="submit" class="btn btn-outline-light">Save changes</button>
+		</div>
+	  </div>
+	  <!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
