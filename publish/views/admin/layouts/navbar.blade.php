05175c8cabbe8363f07192ae944e4db69e60786d
Nikolay <nikolas_@mail.ru>
2020-06-28 19:45:44 +0300

    set flag choose js

--- publish/views/admin/layouts/navbar.blade.php
+++ publish/views/admin/layouts/navbar.blade.php
@@ -114,21 +114,14 @@
       <!-- Language Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="flag-icon flag-icon-us"></i>
+          <i class="flag-icon flag-icon-{{ \App::getLocale() }}"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-right p-0">
-          <a href="#" class="dropdown-item active">
-            <i class="flag-icon flag-icon-us mr-2"></i> English
-          </a>
-          <a href="#" class="dropdown-item">
-            <i class="flag-icon flag-icon-de mr-2"></i> German
-          </a>
-          <a href="#" class="dropdown-item">
-            <i class="flag-icon flag-icon-fr mr-2"></i> French
-          </a>
-          <a href="#" class="dropdown-item">
-            <i class="flag-icon flag-icon-es mr-2"></i> Spanish
-          </a>
+			@foreach (config('languages.languages') as $lang)
+				<a class="dropdown-item {{ $lang === \App::getLocale() ? 'active' : ''}}" href="#" onclick="event.preventDefault(); selectLanguage(this)" data-lang="{{ $lang }}">
+					<i class="flag-icon flag-icon-{{ $lang }} mr-2"></i> {{ $lang }}
+				</a>
+			@endforeach
         </div>
       </li>
       <li class="nav-item">
