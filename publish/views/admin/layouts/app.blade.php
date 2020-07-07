73773a2b45dcfe14e3bb2abedca55e791f033e46
Nikolay <nikolas_@mail.ru>
2020-07-07 11:30:25 +0300

    Delete admin LTE from footer

--- publish/views/admin/layouts/app.blade.php
+++ publish/views/admin/layouts/app.blade.php
@@ -47,11 +47,7 @@
   <!-- /.content-wrapper -->
 
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.2
-    </div>
+	  <div style="text-align: center">Here will my contacts</div>
   </footer>
 
   <!-- Control Sidebar -->
